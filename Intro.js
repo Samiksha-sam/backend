5280e3fd6246bdc2bc14e45ef9e600ef111b3d27
Samiksha-sam <85496009+Samiksha-sam@users.noreply.github.com>
2024-10-19 11:17:30 +0530

    added Intro.js file
    added Intro.js file

---
+++ Intro.js
@@ -0,0 +1,22 @@
+// #Node.js : Node.js® is a free, open-source, cross-platform JavaScript runtime environment
+//cross platform : It can run on any OS like Windows, Linux, Mac etc.
+
+
+
+
+// # Browser : JavaScript Engine -> Responsible for proving JS runtime
+// - Google Chrome : V8 Engine
+// - Firefox : SpiderMonkey
+// - Safari : JavaScriptCore
+// - Edge : Chakra
+
+//Node js is written in C and C++ and it uses Google Chrome's V8 Engine to execute code.
+// Reason : C/C++ is low level language and it is faster than other languages.
+
+//Pros of Node.js
+//  - Node js is excellent for I/O-bound operations.
+//  - non-blocking, event-driven architecture
+
+//  //cons of Node.js
+//     - Not suitable for CPU intensive operations.
+//     - Single threade d.
