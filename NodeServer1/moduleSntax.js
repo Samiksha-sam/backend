2b5cbcb6e22b6dcffae613ed6499c969b222fbbe
Samiksha Ramteke <samiksharamteke2018@gmail.com>
2024-10-26 23:28:33 +0530

    file system module
    file

---
+++ NodeServer1/moduleSntax.js
@@ -0,0 +1,15 @@
+// const http = require('http')
+// const {sum, diff, mul} = require("./modules/calculation.js")
+
+ 
+// import http from 'http'
+// import {sum,diff,mul} from './modules/calculation.js'
+
+// const server = http.createServer((req,res) => {
+//     console.log(sum(10,5))
+//     console.log(diff(10,5))
+//     console.log(mul(10,5))
+//     res.end("Backend !!")
+// })
+
+// server.listen(3000)
--- NodeServer1/modules/calculation.js
+++ NodeServer1/modules/calculation.js
@@ -2,4 +2,4 @@ const sum = (a, b) => a+b
 const diff = (a, b) => a-b
 const mul = (a, b) => a*b
 
-export {sum,diff,mul}
+// export {sum,diff,mul}
