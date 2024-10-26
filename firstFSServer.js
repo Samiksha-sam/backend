44a8ce7aa31a017ee3afe358344439a9e9e3d366
Samiksha Ramteke <samiksharamteke2018@gmail.com>
2024-10-26 11:05:57 +0530

    comiit

--- firstFSServer.js
+++ firstFSServer.js
@@ -2,7 +2,8 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req,res) => {
-    const data = fs.readFileSync('index1.html')
+    const data = fs.readFileSync('index1.html', {encoding : "utf8"})
+    console.log(data)
     res.end(data)
 })
 
