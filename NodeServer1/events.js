b9bfb142e74c835c35e34f80fe1aeb5a6bc5bd36
Samiksha Ramteke <samiksharamteke2018@gmail.com>
2024-11-02 01:18:57 +0530

    events

---
+++ NodeServer1/events.js
@@ -0,0 +1,100 @@
+/**
+ * Event : user activity
+ */
+
+// const http = require('http')
+
+// const server = http.createServer((req,res) => {
+//     if(req.url == '/favicon.ico'){
+//         return res.end()
+//     }
+
+//     const USERS = [
+//         {
+//             "id": 7,
+//             "email": "michael.lawson@reqres.in",
+//             "first_name": "Michael",
+//             "last_name": "Lawson",
+//             "avatar": "https://reqres.in/img/faces/7-image.jpg"
+//           },
+//           {
+//             "id": 8,
+//             "email": "lindsay.ferguson@reqres.in",
+//             "first_name": "Lindsay",
+//             "last_name": "Ferguson",
+//             "avatar": "https://reqres.in/img/faces/8-image.jpg"
+//           },
+//           {
+//             "id": 9,
+//             "email": "tobias.funke@reqres.in",
+//             "first_name": "Tobias",
+//             "last_name": "Funke",
+//             "avatar": "https://reqres.in/img/faces/9-image.jpg"
+//           },
+//           {
+//             "id": 10,
+//             "email": "byron.fields@reqres.in",
+//             "first_name": "Byron",
+//             "last_name": "Fields",
+//             "avatar": "https://reqres.in/img/faces/10-image.jpg"
+//           },
+//           {
+//             "id": 11,
+//             "email": "george.edwards@reqres.in",
+//             "first_name": "George",
+//             "last_name": "Edwards",
+//             "avatar": "https://reqres.in/img/faces/11-image.jpg"
+//           },
+//           {
+//             "id": 12,
+//             "email": "rachel.howell@reqres.in",
+//             "first_name": "Rachel",
+//             "last_name": "Howell",
+//             "avatar": "https://reqres.in/img/faces/12-image.jpg"
+//           }
+//     ]
+//     const DOGS = [
+//         "https://images.dog.ceo/breeds/finnish-lapphund/mochilamvan.jpg",
+//     "https://images.dog.ceo/breeds/sheepdog-indian/Himalayan_Sheepdog.jpg",
+//     "https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_3098.jpg",
+//     "https://images.dog.ceo/breeds/puggle/IMG_081858.jpg",
+//     "https://images.dog.ceo/breeds/bulldog-french/n02108915_2279.jpg",
+//     "https://images.dog.ceo/breeds/hound-ibizan/n02091244_1252.jpg",
+//     "https://images.dog.ceo/breeds/buhund-norwegian/hakon1.jpg",
+//     "https://images.dog.ceo/breeds/leonberg/n02111129_1708.jpg",
+//     "https://images.dog.ceo/breeds/dhole/n02115913_739.jpg",
+//     "https://images.dog.ceo/breeds/terrier-lakeland/n02095570_3113.jpg"
+//     ]
+
+//     if(req.method === 'GET' && req.url === '/users'){
+//         res.end(JSON.stringify(USERS))
+//     }else if(req.method === 'GET' && req.url === '/dogs'){
+//         res.end(JSON.stringify(DOGS))
+//     }else{
+//         res.end('server is listening')
+//     }
+    
+// })
+
+// server.listen(3000)
+
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req,res) => {
+    if(req.url === '/favicon.ico'){
+        return res.end()
+    }
+    if(req.method === 'GET' && req.url==='/form'){
+        const htmlData = fs.readFileSync('forms.html')
+        res.end(htmlData)
+    }else if(req.method === 'POST' && req.url==='/process-data'){
+        console.log('request url is process data')
+        res.end('submitted')
+    }
+    else{
+        res.end('we are learning')
+    }
+})
+
+server.listen(3000)
--- NodeServer1/nodemailer.js
+++ NodeServer1/nodemailer.js
@@ -1,14 +1,58 @@
+/*
+
+    - send mail using node.js through SMTP
+    - SMTP : simple mail transfer protocol 
+*/
+//-----------------------------------------------------------------------------------------
+// const http = require('http')
+// const dotenv = require('dotenv')
+// dotenv.config() //resposible for taking variables from .env to process file
+
+// const server = http.createServer((req,res) => {
+
+//     if(req.url == './favicon.ico'){
+//         return res.end()
+//     }
+//     console.log(process.env.EMAIL)
+//     console.log(process.env.NAME)
+//     res.end('Hi')
+// })
+
+// server.listen(3000)
+//---------------------------------------------------------------------------------------------
+
+/**
+ * https://stackoverflow.com/questions/45478293/username-and-password-not-accepted-when-using-nodemailer
+ * To use nodemailer you need to create a seperate passowd using following link
+ * https://myaccount.google.com/apppasswords
+ */
+
 const http = require('http')
+const nodemailer = require('nodemailer')
 const dotenv = require('dotenv')
-dotenv.config() //resposible for taking variables from .env to process file
 
-const server = http.createServer((req,res) => {
+dotenv.config()
 
-    if(req.url == './favicon.ico'){
+const server = http.createServer(async (req,res) => {
+    if(req.url == '/favicon'){
         return res.end()
     }
-    console.log(process.env.EMAIL)
-    console.log(process.env.NAME)
+
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth :{
+            user:process.env.EMAIL,
+            pass:process.env.PASSWORD
+        }
+    })
+
+    await transporter.sendMail({
+        from: process.env.EMAIL,
+        to:process.env.RECEIVER,
+        subject:'testing mail',
+        text:'This mail is send via nodemailer'
+    })
+
     res.end('Hi')
 })
 
