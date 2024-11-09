e34f9b7cf99de65e26226030572fffc2f0854b89
Samiksha Ramteke <samiksharamteke2018@gmail.com>
2024-11-09 08:53:12 +0530

    events, processing data using postrequest, passing JSON, qs,custom events

--- NodeServer1/events.js
+++ NodeServer1/events.js
@@ -5,6 +5,24 @@
  *  - 'data' : readabble stream receives data/ chunks of data 
  *  - 'end' : reasable stream has no more data to read
  *  - 'error' : Error occured
+ * 
+ * for html data use Body ---> x-www-form-urlencoded
+ * key(name in html) ---> value (actual value)
+ * 
+ * ------------------------------qs-------------------------------
+ * qs.parse(vaiable) : parses html data to Json data
+ * 
+ * 
+ * 
+ * 
+ * --------------------------custom events------------------
+ * created by user to handle specific scenarios
+ * 2 major tasks/expression(listen and execute) : 
+ *  - emit the event
+ *  - listen to events emitted
+ * 
+ * - Importing events module:
+ * 
  */
 
 // const http = require('http')
@@ -82,31 +100,138 @@
 // })
 
 // server.listen(3000)
+//----------------------------------------POST request---------------------------------------------
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req,res) => {
+//     if(req.url === '/favicon.ico'){
+//         return res.end()
+//     }
+//     if(req.method === 'GET' && req.url==='/register'){
+//         const htmlData = fs.readFileSync('forms.html')
+//         res.end(htmlData)
+//     }else if(req.method === 'GET' && req.url === '/about'){
+//         res.end('ABOUT')
+//     }else if(req.method === 'POST' && req.url==='/process-data-test'){
+//         res.end('process-data-test')
+//         req.on('data', (chunk) => {
+//             console.log(chunk.toString())
+//         })
+//     }else if(req.method === 'POST' && req.url==='/process-data'){
+//         res.end('Form Submitted')
+//     }
+//     else{
+//         res.end('we are learning')
+//     }
+// })
+
+//--------------------------------------------------------------------------------------------------
+
+// const http = require('http')
+// const fs = require('fs')
+// const users = []
+
+// const server = http.createServer((req,res) => {
+//     if(req.url=='/favicon.ico'){
+//         return res.end()
+//     }
+//     if(req.method === 'GET' && req.url==='/users'){
+//         res.end(JSON.stringify(users))
+//     }else if(req.method === 'POST' && req.url === '/users'){
+//         let newUser = '';
+
+//         req.on('data',(chunk) => {
+//             newUser += chunk
+//         })
+
+//         req.on('end',() => {
+//             const newUserJson = JSON.parse(newUser)
+//             console.log('Data received from client : ', newUserJson)
+//             users.push(newUserJson)
+//         })
+
+//         req.on('error',() => {
+//             console.log('some error occured')
+//         })
+
+//         res.end('Data received from client successfully')
+//     }else{
+//         res.end('we are learning events :) ')
+//     }
+// })
+
+// server.listen(3000)
+
 
 const http = require('http')
 const fs = require('fs')
+const qs = require('qs')
+const nodemailer = require('nodemailer')
+const dotenv = require('dotenv')
+const EventEmitter = require('events')
+
+dotenv.config()
+const eventEmitter = new EventEmitter();
+
+
+
+const users = []
+
+eventEmitter.on('user-created',async() => {
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth :{
+            user:process.env.EMAIL,
+            pass:process.env.PASSWORD
+        }
+    })
+
+    try {
+        await transporter.sendMail({
+            from: process.env.EMAIL,
+            to: process.env.RECEIVER,
+            subject: 'Testing mail',
+            text: 'This mail is sent via nodemailer'
+        });
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+})
 
 const server = http.createServer((req,res) => {
     if(req.url === '/favicon.ico'){
         return res.end()
     }
-    if(req.method === 'GET' && req.url==='/register'){
-        const htmlData = fs.readFileSync('forms.html')
-        res.end(htmlData)
-    }else if(req.method === 'GET' && req.url === '/about'){
-        res.end('ABOUT')
-    }else if(req.method === 'POST' && req.url==='/process-data-test'){
-        res.end('process-data-test')
+
+    if(req.method === 'GET' && req.url === '/register'){
+       // eventEmitter.emit('test_event')
+        const data = fs.readFileSync('forms.html')
+        res.end(data)
+    }else if(req.method === 'GET' && req.url === '/users'){
+        res.end(JSON.stringify(users))
+    }else if(req.method === 'POST' && req.url==='/users'){
+        let newUser = ''
+
         req.on('data', (chunk) => {
-            console.log(chunk.toString())
+            newUser += chunk
         })
-    
-        
-    }else if(req.method === 'PPOST' && req.url==='/process-data'){
-        res.end('Form Submitted')
+
+        req.on('end',() => {
+            console.log(qs.parse(newUser))
+            // const newUserJSON = JSON.parse(newUser)
+             users.push(qs.parse(newUser))
+             eventEmitter.emit('user-created')
+        })
+
+        req.on('error',() => {
+            console.log('some error occured')
+        })
+
+        res.end('Data received from client successfully')
     }
     else{
-        res.end('we are learning')
+        res.end('we are larninig')
     }
 })
 
