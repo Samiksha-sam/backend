9b5b8a1967ec7a9da532fdb1971c4075eaded3cc
Samiksha Ramteke <samiksharamteke2018@gmail.com>
2024-12-08 10:46:29 +0530

    added MVC model

--- form-project/index.js
+++ form-project/index.js
@@ -3,55 +3,31 @@ const app = express()
 const bodyParser = require('body-parser')
 const port = 3000
 const path = require('path')
-const { 
-    getUsers,
-    checkUserEmailExists,
-    checkUserExists
- } = require('./src/models/user.js')
+ const {
+    displayLandingPage,
+    getUsersData,
+    displaySignUpPage,
+    displayLoginPage,
+    displayDasboardPage,
+    signUpUser,
+    loginUser
+ } = require('./src/controllers/user.js')
 
 app.use(bodyParser.urlencoded())
 
-const USERS = []
+app.get('/', displayLandingPage)
 
-app.get('/signup', (req,res) => {   
-    res.sendFile(path.join(__dirname, 'src/views/signup.html'))
-})
+app.get('/users',getUsersData)
 
-app.get('/login', (req,res) => {
-    res.sendFile(path.join(__dirname, 'src/views/login.html'))
-})
+app.get('/signup', displaySignUpPage)
 
-app.post('/users/signup',(req,res) => {
-    const { name, email, password} = req.body
-    const user = checkUserEmailExists(email)
-    if(user){
-        return res.send('A user with this email already exists.')
-    }
-    const newUser = {name,email,password}
-    USERS.push(newUser)
-    res.redirect('/dashboard')
-})
+app.get('/login', displayLoginPage)
 
-app.post('/users/login',(req,res) => {
-    const { email, password} = req.body
-    const user = checkUserExists(email,password)
-    if(user){
-        res.redirect('/dashboard')
-    }
-    res.send(`You don't have account please signup`)
-})
+app.get('/dashboard',displayDasboardPage)
 
-app.get('/dashboard',(req,res) => {
-    res.sendFile(path.join(__dirname, 'src/views/dashboard.html'))
-})
+app.post('/users/signup',signUpUser)
 
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, 'src/views/landing-page.html'))
-})
-
-app.get('/users',(req,res) => {
-    res.json(getUsers())
-})
+app.post('/users/login', loginUser)
 
 app.listen(3000, () => {
     console.log(`Server is running on port ${port}`)
--- form-project/src/models/user.js
+++ form-project/src/models/user.js
@@ -12,7 +12,12 @@ const checkUserExists = (email, password) => {
     return USERS.find(u => u.email === email && u.password === password) 
 }
 
+const addUser = (newUser) => {
+    USERS.push(newUser)
+}
+
 module.exports = { 
+    addUser,
     getUsers,
     checkUserEmailExists,
     checkUserExists
